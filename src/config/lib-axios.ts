96e0398794be7da17d4c2df17778f128248a75e6
guixinchan <chan249@foxmail.com>
2021-09-28 13:41:46 +0800

    配置proxy

--- src/config/lib-axios.ts
+++ src/config/lib-axios.ts
@@ -1,15 +1,16 @@
 import axios, {AxiosProxyConfig} from "axios";
+import {server} from '../../lib-config.json';
 
 const PROXY: AxiosProxyConfig = {
-    host: '127.0.0.1',
-    port: 8889,
+    host: server.proxy.host,
+    port: server.proxy.port,
 }
 
-    export const URL: string = 'http://libic.fafu.edu.cn';
+export const URL: string = 'http://libic.fafu.edu.cn';
 export const URLP: string = 'http://libic.fafu.edu.cn/ClientWeb/pro/ajax';
 
 // config the proxy of fiddler(otherwise cannot watch the request)
-axios.defaults.proxy = PROXY;
+axios.defaults.proxy = server.proxy.open ? PROXY : null;
 axios.defaults.headers = {
     'Content-type': 'application/json; charset=UTF-8',
     'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.1.2; zh-cn; GT-I9300 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 MicroMessenger/5.2.380',
